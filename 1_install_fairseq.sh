cb789f6f350ca8421612d21286b6cfc8c8b7d86d
literature0305 <31749989+literature0305@users.noreply.github.com>
2023-07-25 17:11:17 +0900

    Update 1_install_fairseq.sh

--- 1_install_fairseq.sh
+++ 1_install_fairseq.sh
@@ -1,4 +1,8 @@
 # Nvidia Docker version: 11.3.1-cudnn8-devel-ubuntu18.04
+# docker run --ipc=host --shm-size 8G -v /home/user/workspace:/home/Workspace -v /DB:/DB -it -d --name fairseq_mepd1 --runtime=nvidia 9ac63d269265
+# apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+# cd /home/Workspace/
+# apt-get update
 
 # install dependencies
 apt-get install git cmake automake autoconf build-essential gawk wget
@@ -12,13 +16,15 @@ wget https://repo.anaconda.com/archive/Anaconda3-2022.10-Linux-x86_64.sh
 git clone https://github.com/facebookresearch/fairseq.git
 rm -r fairseq/.git*
 
-# fairseq_medp/
+# fairseq_medp
 cat medp_scripts/configs.py > fairseq/fairseq/dataclass/configs.py
 cat medp_scripts/train.py > fairseq/fairseq_cli/train.py
 cat medp_scripts/trainer.py > fairseq/fairseq/trainer.py
 cat medp_scripts/fairseq_task.py > fairseq/fairseq/tasks/fairseq_task.py
 cat medp_scripts/label_smoothed_cross_entropy.py > fairseq/fairseq/criterions/label_smoothed_cross_entropy.py
 cat medp_scripts/1_run_iwslt14de2en_segment_level_training.sh > fairseq/1_run_iwslt14de2en_segment_level_training.sh
+cat medp_scripts/1_run_iwslt14de2en_baseline.sh > fairseq/1_run_iwslt14de2en_baseline.sh
+cat medp_scripts/1_run_iwslt14de2en_scheduled_sampling.sh > fairseq/1_run_iwslt14de2en_scheduled_sampling.sh
 cat medp_scripts/2_run_iwslt14de2en_decode.sh > fairseq/2_run_iwslt14de2en_decode.sh
 
 # install fairseq
@@ -28,7 +34,7 @@ pip install --editable ./
 pip install Levenshtein sacremoses
 
 # prepare data
-cd fairseq/examples/translation
+cd examples/translation
 ./prepare-iwslt14.sh
 
 # run script
